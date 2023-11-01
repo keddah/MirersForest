56c081c1bb516a809ca58c24a77d2400bd0844d1
keddah <110899990+keddah@users.noreply.github.com>
2023-11-01 15:11:24 +0000

    Setup++

--- TheForest/TheForest/TheForest.cpp
+++ TheForest/TheForest/TheForest.cpp
@@ -20,21 +20,39 @@ int main()
 		std::cout << SDL_GetError() << std::endl;
 		return 1;
 	}
+
 	window = SDL_CreateWindow(
 		"hello_sdl2",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		screenWidth, screenHeight,
 		SDL_WINDOW_SHOWN
 	);
+
 	if (window == NULL) {
 		std::cout << "could not initialise window!" << std::endl;
 		std::cout << SDL_GetError() << std::endl;
 		return 1;
 	}
+
 	screenSurface = SDL_GetWindowSurface(window);
 	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 144, 238, 144));
 	SDL_UpdateWindowSurface(window);
-	SDL_Delay(2000);
+	
+
+
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_Event e;
+	
+		while (SDL_PollEvent(&e)) 
+		{
+			if (e.type == SDL_QUIT) quit = true;
+		}
+
+	}
+
+
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
