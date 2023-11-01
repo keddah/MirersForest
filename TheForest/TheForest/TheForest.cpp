a7c90976189d00ca96abac08a266e945b0efd28b
keddah <110899990+keddah@users.noreply.github.com>
2023-11-01 15:40:50 +0000

    added some of the essential classes

--- TheForest/TheForest/TheForest.cpp
+++ TheForest/TheForest/TheForest.cpp
@@ -12,33 +12,42 @@ const int screenHeight = 1080;
 
 int main()
 {
-	SDL_Window* window = nullptr;
-	SDL_Surface* screenSurface = nullptr;
-
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "could not initialize SDL2!" << std::endl;
 		std::cout << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	window = SDL_CreateWindow(
+	SDL_Window* window = SDL_CreateWindow(
 		"hello_sdl2",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		screenWidth, screenHeight,
 		SDL_WINDOW_SHOWN
 	);
 
-	if (window == NULL) {
+	if (!window) {
 		std::cout << "could not initialise window!" << std::endl;
 		std::cout << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 144, 238, 144));
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer) {
+		std::cout << "could not initialise window!" << std::endl;
+		std::cout << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+
+	// Make game objects here
+
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+	SDL_RenderFillRect(renderer, NULL);
+
+	SDL_RenderPresent(renderer);
 	SDL_UpdateWindowSurface(window);
 	
-
 
 	bool quit = false;
 	while (!quit)
@@ -51,7 +60,6 @@ int main()
 		}
 
 	}
-
 
 	SDL_DestroyWindow(window);
 	SDL_Quit();
