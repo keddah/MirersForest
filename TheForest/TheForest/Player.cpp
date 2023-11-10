e3cdd4ede0168d085672a672ca9fde95f0a61d78
keddah <deanatkinsonwalker@gmail.com>
2023-11-10 11:54:55 +0000

    renamed folder

--- TheForest/TheForest/Player.cpp
+++ TheForest/TheForest/Player.cpp
@@ -5,14 +5,14 @@ Player::Player()
 {
 	const char* runAnims[]
 	{
-		"Images/run_right1.png",
-		"Images/run_right2.png",
-		"Images/run_right3.png",
-		"Images/run_right4.png",
-		"Images/run_left1.png",
-		"Images/run_left2.png",
-		"Images/run_left3.png",
-		"Images/run_left4.png",
+		"Sprites/run_right1.png",
+		"Sprites/run_right2.png",
+		"Sprites/run_right3.png",
+		"Sprites/run_right4.png",
+		"Sprites/run_left1.png",
+		"Sprites/run_left2.png",
+		"Sprites/run_left3.png",
+		"Sprites/run_left4.png",
 	};
 	ConfigureRenderer(runAnims);
 	renderer.ChangeDestRect(SDL_Rect{10,10,100,100});
